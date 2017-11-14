16ef95dce05d5520ff67d6e94ddbad87b7bd8beb
Mark Daniels <markdaniels@live.co.uk>
2017-11-14 09:06:30 +0000

    Output  update

--- src/genesis/random/Temperature.java
+++ src/genesis/random/Temperature.java
@@ -7,11 +7,11 @@ class Temperature{
     public static void main(String[] arguments){
         
         Scanner KeyBoard = new Scanner(System.in);
-        System.out.print("Please enter the current Temperature in Celcius to convert it to Farenheit and Kelvin: ");
+        System.out.println("Please enter the current Temperature in Celsius to convert it to Farenheit and Kelvin: ");
         double Celsius = KeyBoard.nextDouble();
         double getTempInFarenheit = ((Celsius*9)/5)+32;
         double getTempInKelvin = (Celsius)+273.15;
-        System.out.print("The current Temperature is " + Celsius + " °C " 
+        System.out.println("The current Temperature is " + Celsius + " °C " 
                 + "or " + getTempInFarenheit + " °F "
                 + "or " + getTempInKelvin + " K ");
     }    
