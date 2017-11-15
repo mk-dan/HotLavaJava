95ec5157b61ff212e255e09c0f639d8e46209537
Mark Daniels <markdaniels@live.co.uk>
2017-11-15 09:54:14 +0000

    Naming convention and syntax update

--- src/genesis/random/Temperature.java
+++ src/genesis/random/Temperature.java
@@ -6,12 +6,14 @@ class Temperature{
     
     public static void main(String[] arguments){
         
-        Scanner KeyBoard = new Scanner(System.in);
-        System.out.println("Please enter the current Temperature in Celsius to convert it to Farenheit and Kelvin: ");
-        double Celsius = KeyBoard.nextDouble();
-        double getTempInFarenheit = ((Celsius*9)/5)+32;
-        double getTempInKelvin = (Celsius)+273.15;
-        System.out.println("The current Temperature is " + Celsius + " °C " 
+        Scanner keyBoard = new Scanner(System.in);
+        System.out.println("Please enter a Temperature in Celsius to convert it to Farenheit and Kelvin: ");
+        
+        double celsius = keyBoard.nextDouble();
+        double getTempInFarenheit = ((celsius*9)/5)+32;
+        double getTempInKelvin = (celsius)+273.15;
+        
+        System.out.println("The current Temperature is " + celsius + " °C " 
                 + "or " + getTempInFarenheit + " °F "
                 + "or " + getTempInKelvin + " K ");
     }    
