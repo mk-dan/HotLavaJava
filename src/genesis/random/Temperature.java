3ae106e07b5c8ea18d4fb5d06527b895898bc44a
Mark Daniels <markdaniels@live.co.uk>
2017-11-18 12:45:23 +0000

    added comments

--- src/genesis/random/Temperature.java
+++ src/genesis/random/Temperature.java
@@ -12,7 +12,7 @@ class Temperature{
         LocalDateTime now = LocalDateTime.now();
         int hour = now.get(ChronoField.HOUR_OF_DAY);
         
-        // display greeting
+        // display greeting based on the value of the hour variable
          if (hour < 12) {
              System.out.println("Good morning.\n");
          } else if (hour < 17 ) {
@@ -21,19 +21,21 @@ class Temperature{
              System.out.println("Good evening.\n");
          }
         
-        Scanner keyBoard = new Scanner(System.in);
+        // display enter temperature message
         System.out.println("Please enter a Temperature in Celsius to convert it to Farenheit and Kelvin: ");
         
+        // set variable to hold entered temperature
+        Scanner keyBoard = new Scanner(System.in);
         double celsius = keyBoard.nextDouble();
         double getTempInFarenheit = ((celsius*9)/5)+32;
         double getTempInKelvin = (celsius)+273.15;
         
-        // display the current Temperature in Celsius, Farenheit and Kelvin
+        // display the current temperature in celsius, farenheit and kelvin
         System.out.println("\nThe current Temperature is " + celsius + " °C " 
                 + "\nor \n " + getTempInFarenheit + " °F "
                 + "\nor \n " + getTempInKelvin + " K \n");
         
-        // display a conditional message based on the Temperature entered
+        // display a conditional message based on the temperature entered
         if (celsius <= 10) {
             System.out.println("Wrap Up, its about to be chilly!!\n");
         } else if (celsius >= 30) {
